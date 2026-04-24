e7bccbdbc9a8745b2875dcc0afbbb56652c13cce
Swaiku <prin.jeremy@protonmail.ch>
2026-04-24 08:40:23 +0200

    🎉 Initial commit

---
+++ docs/conf.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2026 Jérémy Prin
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "oberon0-compiler"
+copyright = "2026, Jérémy Prin"
+author = "Jérémy Prin"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_design",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+# Furo theme options
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#0071c5",
+        "color-brand-content": "#0071c5",
+    },
+}
---
+++ docs/index.rst
@@ -0,0 +1,47 @@
+oberon0-compiler documentation
+=============================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+A Oberon-0 compiler
+
+.. grid:: 1 2 2 2
+    :gutter: 2
+
+    .. grid-item-card:: 📚 Module Documentation
+        :link-type: ref
+        :link: modules-section
+
+        Explore the API documentation and module reference.
+
+    .. grid-item-card:: 🔍 API Reference
+        :link: genindex
+
+        Complete index of all classes, functions, and variables.
+
+
+.. toctree::
+   :maxdepth: 2
+   :hidden:
+
+.. _modules-section:
+
+Modules
+=======
+
+.. toctree::
+   :maxdepth: 3
+
+.. automodule:: oberon0_compiler
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
